c350cff77b8e987780235e7ec42abdac316d0829
Kotik <62649860+XDENIX@users.noreply.github.com>
2020-08-22 16:29:22 +0200

    Add files via upload

---
+++ commands/info/help.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require("discord.js"); 
+module.exports = {
+name: 'help',
+aliases:['h'],
+description_en: 'List commands',
+description_ru: 'Список доступных команд',
+usages: ['[commands]','help'],
+uses: 0,
+category: 'info',
+async run(bot, message, args) {
+let embed = new MessageEmbed()
+.setAuthor(message.author.username,message.author.avatarURL())
+.setDescription(`Все мои команды
+${bot.commands.filter(c => c.category === 'info').map(c => `\`${process.env.prefix}${c.name}\``).join(', ')}`)
+.addField(`Ссылки`,`Сайт: http://localhost:${process.env.port}
+ Сервер поддержки: https://discord.gg/Eh9thsa
+ Мой код на гитхабе: https://github.com/XDENIX/API`)
+.addField(`Мой разработчик`,`**${bot.users.cache.get(process.env.owner).tag}**`)
+message.channel.send(embed)
+}
+}
